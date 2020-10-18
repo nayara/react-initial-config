f8b03f4804c15d760b8814c8490b68e4b1a4c8f7
Nayara Souza <nsouza@thoughtworks.com>
2020-10-18 13:21:44 -0300

    Init config for react app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const root = document.getElementById('root');
+
+const App = () => <h1> Hello from React! </h1>;
+
+ReactDom.render(<App />, root);
